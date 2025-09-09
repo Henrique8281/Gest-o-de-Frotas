9e4856421482f12240453e3c7a6ab5e4b0299f99
Henrique8281 <carlos.henrique87@aluno.ifce.edu.br>
2025-09-09 18:50:43 -0300

    criaçao de rotas
    rotas, não consegui instalar o flask

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from flask import flask, render_templates
 app = flask(__name__)
 
 @app.route('/login')
-def tela_login():
+def tela_usuario():
     return render_templates('login.html')
 
 @app.route('/homepage')
-def tela_inicial():
+def tela_usuario():
+    return render_templates('homepage.html')
+
+@app.route('/lista_veiculos')
+def tela_veiculos():
+    return render_templates('lista_veiculos.html')
+
+@app.route('/registroveiculos')
+def tela_veiculos():
+    return render_templates('registro_veiculos.html')
+
+@app.route('/manutencao')
+def tela_veiculos():
+    return render_templates('registro_manutencao.html')
+
+@app.route('/homepage')
+def tela_manutencao():
     return render_templates('homepage.html')
 
 if __name__ == '__main__':
