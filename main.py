a814470668a9c15345de165d9d3ded2cde8d3f14
Henrique8281 <carlos.henrique87@aluno.ifce.edu.br>
2025-09-13 23:07:08 -0300

    aplicacao do Flask
    finalmente instalaçao do flask e correção de rotas

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-from flask import flask, render_templates
+from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/login')
-def tela_usuario():
-    return render_templates('login.html')
+def tela_um():
+    return render_template('/usuario/login.html')
 
 @app.route('/homepage')
-def tela_usuario():
-    return render_templates('homepage.html')
+def tela_dois():
+    return render_template('/usuario/homepage.html')
 
 @app.route('/lista_veiculos')
-def tela_veiculos():
-    return render_templates('lista_veiculos.html')
+def tela_4():
+    return render_template('/veiculos/lista_veiculos.html')
 
 @app.route('/registroveiculos')
-def tela_veiculos():
-    return render_templates('registro_veiculos.html')
+def tela_5():
+    return render_template('/veiculos/registro_veiculos.html')
 
 @app.route('/manutencao')
-def tela_veiculos():
-    return render_templates('registro_manutencao.html')
+def tela_6():
+    return render_template('/veiculos/registro_manutencao.html')
 
-@app.route('/homepage')
-def tela_manutencao():
-    return render_templates('homepage.html')
+if __name__ =='__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
-if __name__ == '__main__':
-    app.run()
